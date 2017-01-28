4cf1e248087c38365886e053a90bf82eb2080e07
nathan-j-brenner <nathan.j.brenner@gmail.com>
2017-01-28 14:34:54 -0800

    drafted auth service

---
+++ template/core-dist/src/app/authentication/auth.config.ts
@@ -0,0 +1,9 @@
+interface AuthConfiguration {
+    clientID: string,
+    domain: string
+}
+
+export const myConfig: AuthConfiguration = {
+    clientID: 'Mdrg6SXsh4o0hglFrltqX9OTvZN5JTkt',
+    domain: 'brenner.auth0.com'
+};
--- template/core-dist/src/app/authentication/authentication.service.ts
+++ template/core-dist/src/app/authentication/authentication.service.ts
@@ -1,11 +1,31 @@
 import {Injectable} from '@angular/core';
 import {Http} from '@angular/http';
 import {BaseService} from '../shared/services/base.service';
+import { tokenNotExpired } from 'angular2-jwt';
+import { myConfig } from './auth.config';
+
+declare var Auth0Lock: any;
 
 @Injectable()
 export class AuthenticationService extends BaseService {
+  lock = new Auth0Lock(myConfig.clientID, myConfig.domain, {});
 
   constructor(protected _http: Http) {
     super(_http);
+    this.lock.on('authenticated', (authResult) => {
+      localStorage.setItem('id_token', authResult.idToken)
+    });
+  }
+
+  public login() {
+    this.lock.show();
+  };
+
+  public authenticated(){
+    return tokenNotExpired();
+  }
+
+  public logout(){
+    localStorage.removeItem('id_token');
   }
 }
