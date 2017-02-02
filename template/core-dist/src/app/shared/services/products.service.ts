bca79e8519f9213001b2f3fb98afca38283a0f49
Nikola Bosic <nikola.bosic@coingcs.com>
2017-02-02 12:35:29 +0100

    Add first graphql query: products index.

--- template/core-dist/src/app/shared/services/products.service.ts
+++ template/core-dist/src/app/shared/services/products.service.ts
@@ -1,24 +1,42 @@
 import { Injectable } from '@angular/core';
 import {Product} from "../models/product.model";
 import {Subject} from "rxjs";
+import {Http, Headers} from "@angular/http";
 
 @Injectable()
 export class ProductsService {
 
-  private productsData: Product[] = [
-    new Product({id: '1111', name: 'first', sku: 'first sku'}),
-    new Product({id: '2222', name: 'second', sku: 'second sku'}),
-    new Product({id: '3333', name: 'third', sku: 'third sku'})
-  ];
-
+  private auth =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiOTNiMDg2YjgtNjM0My00MjcxLTg3ZDYtYjJhMDAxNDlmMDcwIiwiaWF0IjoxNDg2MDI3Mzc2fQ.WWso40RRK-xHIvMOm2NEFGEnQHkJH2KQq_FWShkQ0GM';
+  private allProductsQuery =
+  `{
+        productlist{
+          products {
+            id,
+            name,
+            sku
+          }
+        }
+       }`;
+  private endpoint = 'https://9xq4cs3cq1.execute-api.us-east-1.amazonaws.com/development/graph';
   products$: Subject<Product[]>;
 
-  constructor() {
+  constructor(private http: Http) {
     this.products$ = new Subject<Product[]>();
   }
 
   getProducts(): void {
-    this.products$.next(this.productsData);
+    let head = new Headers();
+    head.append('Authorization', this.auth);
+
+    this.http.post(this.endpoint, this.allProductsQuery, { headers: head }).subscribe(
+      (data) => {
+        this.products$.next(data.json().data.productlist.products);
+      },
+      (error) => {
+        console.log('ERROR', error);
+      }
+    );
   }
 
 }
