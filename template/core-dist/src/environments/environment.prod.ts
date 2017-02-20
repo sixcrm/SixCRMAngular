2d025e9cdf1c81dd68ce9be4e5d90fb781c212df
Nikola <nikolabosic91@gmail.com>
2017-02-20 19:24:39 +0100

    Change API endpoint to https://api.sicrm.com/graph.

--- template/core-dist/src/environments/environment.prod.ts
+++ template/core-dist/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  endpoint: 'https://9xq4cs3cq1.execute-api.us-east-1.amazonaws.com/development/graph',
+  endpoint: 'https://api.sixcrm.com/graph',
   clientID: 'JM1tC2j7tycbu62el3oBhyklpNbk5x6F',
   domain: 'sixcrm.auth0.com',
   auth0RedirectUrl: 'https://admin.sixcrm.com'
