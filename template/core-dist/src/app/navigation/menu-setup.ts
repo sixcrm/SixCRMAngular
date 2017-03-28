f978d72542cdaa562179e02e943b9666b00db6e6
Nikola Bosic <nikola.bosic@coingcs.com>
2017-03-28 16:34:18 +0200

    Change routes to lower case.

--- template/core-dist/src/app/navigation/menu-setup.ts
+++ template/core-dist/src/app/navigation/menu-setup.ts
@@ -15,12 +15,12 @@ export const menuItemSetup: MenuItem[] = [
 
   new MenuItem('Customers', null, [
     new MenuItem('Customers', 'customers'),
-    new MenuItem('Credit Cards', 'creditCards')
+    new MenuItem('Credit Cards', 'creditcards')
   ]).setIcon('account_box'),
 
   new MenuItem('CRM', null, [
     new MenuItem('Products', 'products'),
-    new MenuItem('Product Schedules', 'productSchedule'),
+    new MenuItem('Product Schedules', 'productschedule'),
     new MenuItem('Campaigns', null, [
       new MenuItem('Campaigns', 'campaigns'),
       new MenuItem('Emails', 'emails'),
@@ -28,14 +28,14 @@ export const menuItemSetup: MenuItem[] = [
     new MenuItem('Affiliates', 'affiliates'),
     new MenuItem('Sessions', 'sessions'),
     new MenuItem('3rd Party Providers', null, [
-      new MenuItem('Fulfillment Providers', 'fulfillmentProviders'),
-      new MenuItem('SMTP Providers', 'smtpProviders'),
+      new MenuItem('Fulfillment Providers', 'fulfillmentproviders'),
+      new MenuItem('SMTP Providers', 'smtpproviders'),
     ]),
   ]).setIcon('account_balance'),
 
   new MenuItem('Merchants', null, [
-    new MenuItem('Merchant Providers', 'merchantProviders'),
-    new MenuItem('Load Balancers', 'loadBalancers'),
+    new MenuItem('Merchant Providers', 'merchantproviders'),
+    new MenuItem('Load Balancers', 'loadbalancers'),
   ]).setIcon('payment'),
 
   new MenuItem('Settings', null, [
@@ -71,7 +71,7 @@ export function menuItems(authService: AuthenticationService): MenuItem[] {
     customerItems.push(new MenuItem('Customers', 'customers'));
   }
   if (authService.hasPermissions('creditcard', 'view')) {
-    customerItems.push(new MenuItem('Credit Cards', 'creditCards'));
+    customerItems.push(new MenuItem('Credit Cards', 'creditcards'));
   }
   if (customerItems.length > 0) {
     items.push(new MenuItem('Customers', null, customerItems).setIcon('account_box'));
@@ -83,7 +83,7 @@ export function menuItems(authService: AuthenticationService): MenuItem[] {
     crmItems.push(new MenuItem('Products', 'products'));
   }
   if (authService.hasPermissions('productschedule', 'view')) {
-    crmItems.push(new MenuItem('Product Schedules', 'productSchedule'));
+    crmItems.push(new MenuItem('Product Schedules', 'productschedule'));
   }
 
   // Add campaign and email menu items to CRM menu
@@ -110,10 +110,10 @@ export function menuItems(authService: AuthenticationService): MenuItem[] {
   // Add 3rd party providers to CRM menu
   let thirdPartyProviders: MenuItem[] = [];
   if (authService.hasPermissions('fulfillmentprovider', 'view')) {
-    thirdPartyProviders.push(new MenuItem('Fulfillment Providers', 'fulfillmentProviders'))
+    thirdPartyProviders.push(new MenuItem('Fulfillment Providers', 'fulfillmentproviders'))
   }
   if (authService.hasPermissions('smtpprovider', 'view')) {
-    thirdPartyProviders.push(new MenuItem('SMTP Providers', 'smtpProviders'))
+    thirdPartyProviders.push(new MenuItem('SMTP Providers', 'smtpproviders'))
   }
   if (thirdPartyProviders.length > 0) {
     crmItems.push(new MenuItem('3rd Party Providers', null, thirdPartyProviders));
@@ -126,10 +126,10 @@ export function menuItems(authService: AuthenticationService): MenuItem[] {
   // Add Merchants menu item
   let merchants: MenuItem[] = [];
   if (authService.hasPermissions('merchantprovider', 'view')) {
-    merchants.push(new MenuItem('Merchant Providers', 'merchantProviders'))
+    merchants.push(new MenuItem('Merchant Providers', 'merchantproviders'))
   }
   if (authService.hasPermissions('loadbalancer', 'view')) {
-    merchants.push(new MenuItem('Load Balancers', 'loadBalancers'))
+    merchants.push(new MenuItem('Load Balancers', 'loadbalancers'))
   }
 
   if (merchants.length > 0) {
