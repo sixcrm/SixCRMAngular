409b21ccb552f2ba185f7f1e2a5ad7e7f32bb670
Nikola Bosic <nikola.bosic@coingcs.com>
2017-04-26 18:00:17 +0200

    Redesign transaction view

--- src/app/shared/models/processor-response.model.ts
+++ src/app/shared/models/processor-response.model.ts
@@ -3,11 +3,20 @@ import {Entity} from './entity.interface';
 export class ProcessorResponse implements Entity<ProcessorResponse> {
   id: string = '';
   message: string = '';
+  authCode: string = '';
+  responseText: string = '';
+  transactionId: string = '';
 
   constructor(obj?: any) {
     if (obj) {
       obj = JSON.parse(obj);
       this.message = obj.message || '';
+
+      if (obj.results) {
+        this.responseText = obj.results.responsetext || '';
+        this.authCode = obj.results.authcode || '';
+        this.transactionId = obj.results.transactionid || '';
+      }
     }
   }
 
