2157151bb0b79e8301420c310892d421f2836c2a
Nikola Bosic <nikola.bosic@coingcs.com>
2017-04-27 18:26:45 +0200

    Initial work on new dashboard

--- src/app/pages/dashboard-page/dashboard.component.ts
+++ src/app/pages/dashboard-page/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {utc} from 'moment';
 
 @Component({
   selector: 'c-dashboard',
@@ -7,70 +8,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  public sales: any = [];
+  filterTerms: any[] = [];
+  currentFilterTerm: string;
 
-  public pieChartLabels:string[] = ['label 1', 'label 2', 'label 3'];
-  public pieChartData:number[] = [75, 20, 5];
-  public pieChartType:string = 'pie';
+  dateFilters: any = [
+    {label: '1M', start: utc().subtract(1,'M'), end: utc()},
+    {label: '3M', start: utc().subtract(3,'M'), end: utc()},
+    {label: '6M', start: utc().subtract(6,'M'), end: utc()},
+    {label: 'YTD', start: utc().startOf('year'), end: utc()},
+    {label: '1Y', start: utc().subtract(1,'y'), end: utc()},
+    {label: 'ALL', start: utc().subtract(10,'y'), end: utc()}
+  ];
 
-  public barChartOptions:any = {
-    scaleShowVerticalLines: false,
-    responsive: true
+  activeDataFilterIndex: number = 1;
+
+  chartOptions = {
+    credits: {enabled: false},
+    rangeSelector: {enabled: false},
+    series: [{
+      data: [
+        [1266278400000,29.06],
+        [1266364800000,28.94],
+        [1266451200000,28.99],
+        [1266537600000,28.81],
+        [1266796800000,28.63],
+        [1266883200000,28.15],
+        [1266969600000,28.66]
+    ]}]
   };
-  public barChartLabels:string[] = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'];
-  public barChartType:string = 'bar';
-
-  public barChartData:any[] = [
-    {data: [2200, 1450, 1700, 1490, 2050, 1550], label: 'Label 1'}
-  ];
-
-  public chartColors:Array<any> = [
-    {
-      backgroundColor: '#4C91DE',
-      borderColor: '#4C91DE',
-      pointBackgroundColor: '#4C91DE',
-      pointBorderColor: '#4C91DE',
-      pointHoverBackgroundColor: '#4C91DE',
-      pointHoverBorderColor: '#4C91DE'
-    }
-  ];
-
-  public lineChartData:Array<any> = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [50, 65, 83, 70, 40, 45, 50], label: 'Series A'},
-  ];
-  public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-  public lineChartOptions:any = {
-    responsive: true
-  };
-
-  public lineChartType:string = 'line';
-  public lineChartColors:Array<any> = [
-    {
-      backgroundColor: 'rgba(83,165,218, 0.5)',
-      borderColor: 'rgba(83,165,218, 0.5)',
-      pointBackgroundColor: 'rgba(83,165,218, 0.5)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(83,165,218, 0.5)'
-    },
-    {
-      backgroundColor: 'rgba(158,231,252, 0.5)',
-      borderColor: 'rgba(158,231,252, 0.5)',
-      pointBackgroundColor: 'rgba(158,231,252, 0.5)',
-      pointBorderColor: 'rgba(158,231,252, 0.5)',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: '#fff'
-    }
-  ];
 
   constructor() { }
 
-  ngOnInit() {
-    for (let i = 0; i < 10; i++) {
-      let obj = {campaign: 'Campaign 32', product: 'MygoodProduct', link: 'www.goodproduct.com', rebill: 'Rebill - $49.44'};
-      this.sales.push(obj);
+  ngOnInit() { }
+
+  removeFilterTerm(filterTerm: string): void {
+    let index = this.filterTerms.indexOf(filterTerm);
+
+    if (index > -1) {
+      this.filterTerms.splice(index,1);
     }
+  }
+
+  filterTermInput(event): void {
+    this.currentFilterTerm = event.target.value;
+  }
+
+  onFilterKey(event): void {
+    if (event.key === 'Enter' && this.currentFilterTerm) {
+      this.filterTerms.push(this.currentFilterTerm);
+      this.currentFilterTerm = '';
+    }
+  }
+
+  setActiveDateFilterIndex(index: number): void {
+    this.activeDataFilterIndex = index;
   }
 
 }
--- src/app/pages/dashboard-page/dashboard.module.ts
+++ src/app/pages/dashboard-page/dashboard.module.ts
@@ -3,8 +3,13 @@ import { CommonModule } from '@angular/common';
 import {dashboardRouting} from './dashboard.routing';
 import {MaterialModule} from '@angular/material';
 import {DashboardComponent} from './dashboard.component';
-import {ChartsModule} from 'ng2-charts';
 import {SharedModule} from '../../shared/shared.module';
+import {ChartModule} from 'angular2-highcharts';
+import {HighchartsStatic} from 'angular2-highcharts/dist/HighchartsService';
+
+export function highchartsFactory() {
+  return require('highcharts/highstock');
+}
 
 @NgModule({
   imports : [
@@ -12,15 +17,17 @@ import {SharedModule} from '../../shared/shared.module';
     SharedModule,
     CommonModule,
     MaterialModule.forRoot(),
-    ChartsModule
+    ChartModule
   ],
   declarations : [
     DashboardComponent
   ],
   exports : [
   ],
-  providers: [
-  ]
+  providers: [{
+    provide: HighchartsStatic,
+    useFactory: highchartsFactory
+  }],
 })
 export class DashboardModule {
 }
