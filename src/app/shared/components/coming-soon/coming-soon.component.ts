06cbc7abc625cba6e64d90e8244cd8bdb41a5575
Nikola Bosic <nikola.bosic@coingcs.com>
2017-05-12 17:52:08 +0200

    Add back button to 'in development' page

--- src/app/shared/components/coming-soon/coming-soon.component.ts
+++ src/app/shared/components/coming-soon/coming-soon.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NavigationService} from '../../../navigation/navigation.service';
 
 @Component({
   selector: 'coming-soon',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComingSoonComponent implements OnInit {
 
-  constructor() { }
+  constructor(private navigation: NavigationService) { }
 
   ngOnInit() {
   }
