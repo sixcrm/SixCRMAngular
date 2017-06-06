fa81deb556df646e266d4468084e86fb570d5c2b
Nikola Bosic <nikola.bosic@coingcs.com>
2017-06-06 15:31:24 +0200

    Fix null pointer exception when retrieving search data

--- src/app/shared/services/abstract-entity.service.ts
+++ src/app/shared/services/abstract-entity.service.ts
@@ -147,10 +147,10 @@ export abstract class AbstractEntityService<T> {
         let listKey = Object.keys(json)[0];
         let listData = json[listKey];
 
-        let entitiesKey = Object.keys(listData)[0];
-        let entitiesData = listData[entitiesKey];
+        let entitiesKey = listData ? Object.keys(listData)[0] : null;
+        let entitiesData = entitiesKey ? listData[entitiesKey] : null;
 
-        if (listData.pagination) {
+        if (listData && listData.pagination) {
           this.entitiesHasMore$.next(listData.pagination.end_cursor !== '' && listData.pagination.has_next_page);
           this.cursor = listData.pagination.end_cursor;
         }
