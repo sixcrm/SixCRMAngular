dddc66f40e31f2fba5d1c60746f5f0e50f140e48
Nikola Bosic <nikola.bosic@coingcs.com>
2017-08-07 18:02:40 +0200

    Fix merge script

--- deployment/merge_branches.sh
+++ deployment/merge_branches.sh
@@ -5,30 +5,11 @@ set -o pipefail
 
 git fetch --all
 
-case "$2" in
-	production)
-		git fetch
-		GIT_COMMIT=$(git rev-parse HEAD)
-		echo "Git commit: $GIT_COMMIT"
-		TAGS=$(git describe --all --exact-match $GIT_COMMIT)
-		echo "Tags:"
-		echo $TAGS;
+echo "Checking out..."
+git checkout $2
 
-		echo "$TAGS" | grep -q "tags/v\d\+.*"
-		if [ $? -eq 0 ]; then
-		  echo "Version tag present.  Merging to production."
-		  git checkout $2
-		  git merge $1
-		  git push origin $2
-		else
-		  echo "Version tag not present.  Skipping merge to production."
-		fi
+echo "Merging..."
+git merge $1
 
-	;;
-	*)
-		echo "Skipping version tagging requirements for merge.";
-		git checkout $2
-		git merge $1
-		git push origin $2
-	;;
-esac
+echo "Pushing..."
+git push origin $2
