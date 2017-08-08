5ff0951cad8b2bcca1f3b1ad5d6e3511fd736bbc
Nikola Bosic <nikola.bosic@coingcs.com>
2017-08-08 16:29:44 +0200

    Enable decimals when adding price for schedule

--- src/app/shared/utils/form.utils.ts
+++ src/app/shared/utils/form.utils.ts
@@ -9,6 +9,17 @@ export function isAllowedNumeric(event): boolean {
   return true;
 }
 
+export function isAllowedCurrency(event): boolean {
+  const pattern = /[0-9]|\.|Backspace|ArrowRight|ArrowLeft/;
+
+  if (!pattern.test(event.key)) {
+    event.preventDefault();
+    return false;
+  }
+
+  return true;
+}
+
 export function isAllowedFloatNumeric(event): boolean {
   const pattern = /[0-9]|\.|Backspace|ArrowRight|ArrowLeft/;
 
