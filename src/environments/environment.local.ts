a055eda870ee62a614aef622f4c5ac08dab4996b
Nikola Bosic <nikola.bosic@coingcs.com>
2017-08-08 15:59:04 +0200

    Remove sliders

--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -5,8 +5,8 @@
 
 export const environment = {
   production: false,
-  endpoint: 'https://development-api.sixcrm.com/graph/',
-  jwtEndpoint: 'https://development-api.sixcrm.com/token/acquire',
+  endpoint: 'https://api.sixcrm.com/graph/',
+  jwtEndpoint: 'https://api.sixcrm.com/token/acquire',
   clientID: 'JM1tC2j7tycbu62el3oBhyklpNbk5x6F',
   domain: 'sixcrm.auth0.com',
   auth0RedirectUrl: 'http://localhost:4200',
