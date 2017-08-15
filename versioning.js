35abf6621f38a67b08d5ea26fc27bf2e052e1720
Nikola <nikolabosic91@gmail.com>
2017-08-15 20:55:31 +0200

    Fix versioning script

--- versioning.js
+++ versioning.js
@@ -1,17 +1,31 @@
 var fs = require('fs');
+var exec = require('child_process').exec;
 
 var env = process.argv[3] === 'prod' ? '.prod' : (process.argv[3] === 'stage' ? '.stage' : '.dev');
 var envFile = './src/environments/environment' + env +'.ts';
+var version;
 
-fs.readFile(envFile, 'utf8', function (err,data) {
+exec('git rev-parse HEAD', (err, stdout) => {
   if (err) {
-    return console.log(err);
-  }
+    console.log(err);
+    process.exit(1);
+  };
 
-  var name = process.argv[2].substring(0, 8);
-  var result = data.replace(/0.0.0/g, name);
+  version = stdout.trim().substring(0, 8);
+  updateVersion();
+})
 
-  fs.writeFile(envFile, result, 'utf8', function (err) {
-    if (err) return console.log(err);
+function updateVersion() {
+  fs.readFile(envFile, 'utf8', function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+
+    var result = data.replace(/0.0.0/g, version);
+
+    fs.writeFile(envFile, result, 'utf8', function (err) {
+      if (err) return console.log(err);
+    });
   });
-});
+
+}
