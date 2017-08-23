033f7bd2225294c0c2c88194bb5c50affabe0649
Nikola Bosic <nikola.bosic@coingcs.com>
2017-08-23 13:33:13 +0200

    Refactor autocomplete dropdown fields

--- src/app/shared/components/autocomplete-input/autocomplete-input.component.ts
+++ src/app/shared/components/autocomplete-input/autocomplete-input.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, Input, Output, EventEmitter, ElementRef} from '@angular/core';
+import {Component, OnInit, Input, Output, EventEmitter, ElementRef, ViewChildren, ViewChild, QueryList} from '@angular/core';
 
 @Component({
   selector: 'autocomplete-input',
@@ -8,21 +8,38 @@ import {Component, OnInit, Input, Output, EventEmitter, ElementRef} from '@angul
 })
 export class AutocompleteInputComponent implements OnInit {
 
+  @ViewChildren('autocompleteOption') optionFields: QueryList<ElementRef>;
+  @ViewChild('optionsContainer') optionsContainer: ElementRef;
+
   currentValue: string;
   initValue: string;
   showOptions: boolean;
+  allOptions: any[] = [];
+  filteredOptions: any[] = [];
+
+  @Input() mapFunction = (el: any) => el;
 
   @Input() set initialValue(value: string) {
     this.initValue = value;
-    this.currentValue = value;
+    this.currentValue = this.mapFunction(value);
+    this.filterOptions();
   };
-  @Input() options: string[];
+  @Input() set options(opts: any[]) {
+    if (!opts) return;
+
+    this.allOptions = opts.slice();
+
+    if (this.currentValue) {
+      this.filterOptions();
+    }
+  }
   @Input() placeholder: string;
   @Input() showCancelButton: boolean = true;
-  @Input() mapFunction = (el: any) => el;
   @Input() disabled: boolean = false;
   @Input() required: boolean = false;
   @Output() selected: EventEmitter<any> = new EventEmitter();
+
+  focusedOptionIndex: number = 0;
 
   constructor(private elementRef: ElementRef) { }
 
@@ -43,5 +60,86 @@ export class AutocompleteInputComponent implements OnInit {
     if (!this.elementRef.nativeElement.contains(event.target)) {
       this.showOptions = false;
     }
+  }
+
+  blurred() {
+    this.emitSelected();
+  }
+
+  focused() {
+    this.showOptions = true;
+    this.filterOptions();
+  }
+
+  emitSelected() {
+    const selectedValue = this.filteredOptions[this.focusedOptionIndex];
+
+    if (!selectedValue) {
+      this.currentValue = this.mapFunction(this.initValue);
+    } else {
+      this.currentValue = this.mapFunction(selectedValue);
+      this.selected.emit(selectedValue);
+    }
+
+    this.showOptions = false;
+  }
+
+  onKey(keyboardEvent) {
+    switch (keyboardEvent.key) {
+      case 'Enter': {}
+      case 'Tab': {
+        this.emitSelected();
+        break;
+      }
+      case 'ArrowDown': {
+        this.focusedOptionIndex++;
+        if (this.focusedOptionIndex >= this.filteredOptions.length) {
+          this.focusedOptionIndex = 0;
+        }
+        this.ensureOptionVisible();
+        break;
+      }
+      case 'ArrowUp': {
+        this.focusedOptionIndex--;
+        if (this.focusedOptionIndex < 0) {
+          this.focusedOptionIndex = this.filteredOptions.length - 1;
+        }
+        this.ensureOptionVisible();
+        break;
+      }
+      default: {
+        this.showOptions = true;
+        this.focusedOptionIndex = 0;
+      }
+    }
+  }
+
+  change(value) {
+    this.currentValue = value;
+    this.filterOptions();
+  }
+
+  ensureOptionVisible() {
+    const option = this.optionFields.toArray()[this.focusedOptionIndex];
+    if (option) {
+      const nativeEl = option.nativeElement;
+
+      if ( (nativeEl.offsetTop + nativeEl.offsetHeight) > this.optionsContainer.nativeElement.scrollTop + this.optionsContainer.nativeElement.offsetHeight ){
+        this.optionsContainer.nativeElement.scrollTop = nativeEl.offsetTop + nativeEl.offsetHeight - this.optionsContainer.nativeElement.offsetHeight;
+      } else if (nativeEl.offsetTop < this.optionsContainer.nativeElement.scrollTop) {
+        this.optionsContainer.nativeElement.scrollTop = nativeEl.offsetTop;
+      }
+    }
+  }
+
+  filterOptions() {
+    this.filteredOptions = (() => {
+      if (!this.allOptions || this.allOptions.length === 0 || !this.mapFunction) return [];
+      if (!this.currentValue) return this.allOptions;
+
+      const fs = this.currentValue.toUpperCase();
+
+      return this.allOptions.filter(s => this.mapFunction(s).toUpperCase().indexOf(fs) !== -1);
+    })()
   }
 }
