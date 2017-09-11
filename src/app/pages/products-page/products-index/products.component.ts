d140d2267f09b1647239afc3c64c005553a098fb
Nikola Bosic <nikola.bosic@coingcs.com>
2017-09-11 17:54:41 +0200

    Products can't be deleted from index page when associated to product schedule

--- src/app/pages/products-page/products-index/products.component.ts
+++ src/app/pages/products-page/products-index/products.component.ts
@@ -7,6 +7,13 @@ import {PaginationService} from '../../../shared/services/pagination.service';
 import {AuthenticationService} from '../../../authentication/authentication.service';
 import {ActivatedRoute, Router} from '@angular/router';
 import {ColumnParams} from '../../../shared/models/column-params.model';
+import {ProductScheduleService} from '../../../shared/services/product-schedule.service';
+import {CustomServerError} from '../../../shared/models/errors/custom-server-error';
+import {
+  productScheduleListQuery,
+  productSchedulesByProduct
+} from '../../../shared/utils/queries/entities/product-schedule.queries';
+import {MessageDialogComponent} from '../../message-dialog.component';
 
 @Component({
   selector: 'products',
@@ -21,7 +28,8 @@ export class ProductsComponent extends AbstractEntityIndexComponent<Product> imp
     dialog: MdDialog,
     paginationService: PaginationService,
     router: Router,
-    activatedRoute: ActivatedRoute
+    activatedRoute: ActivatedRoute,
+    private productScheduleService: ProductScheduleService
   ) {
     super(productsService, auth, dialog, paginationService, router, activatedRoute);
 
@@ -33,11 +41,37 @@ export class ProductsComponent extends AbstractEntityIndexComponent<Product> imp
     ];
   }
 
+  deleteEntity(id: string) {
+    this.productScheduleService.indexQuery = (limit?:number, cursor?:string) => productSchedulesByProduct(id, limit, cursor);
+
+    this.productScheduleService.entities$.take(1).takeUntil(this.unsubscribe$).subscribe(schedules => {
+      if (schedules instanceof CustomServerError) return;
+
+      if (schedules && schedules.length > 0) {
+        this.showMessageDialog('You can not delete this product as long as it is associated with a product schedule.');
+      } else {
+        super.deleteEntity(id);
+      }
+    });
+    this.productScheduleService.getEntities();
+  }
+
+  showMessageDialog(text: string) {
+    let messageDialogRef = this.deleteDialog.open(MessageDialogComponent);
+    messageDialogRef.componentInstance.text = text;
+
+    messageDialogRef.afterClosed().take(1).subscribe(() => {
+      messageDialogRef = null;
+    });
+  }
+
   ngOnInit() {
     this.init();
   }
 
   ngOnDestroy() {
+    this.productScheduleService.indexQuery = productScheduleListQuery;
+
     this.destroy();
   }
 }
