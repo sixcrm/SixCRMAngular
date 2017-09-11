b8a803f486399348e9872b56b96093bb31c94eba
Nikola Bosic <nikola.bosic@coingcs.com>
2017-09-11 16:11:27 +0200

    Fix responsiveness of top nav bar and entity index view

--- src/app/pages/campaigns-page/campaigns-index/campaigns.component.ts
+++ src/app/pages/campaigns-page/campaigns-index/campaigns.component.ts
@@ -28,7 +28,6 @@ export class CampaignsComponent extends AbstractEntityIndexComponent<Campaign> i
 
     let f = this.authService.getTimezone();
     this.columnParams = [
-      new ColumnParams('ID', (e: Campaign) => e.id),
       new ColumnParams('Name', (e: Campaign) => e.name),
       new ColumnParams('Total products', (e: Campaign) => e.productSchedules.map(p => p.schedules.length).reduce((a, b) => a+b, 0) + '', 'right'),
       new ColumnParams('Total Scheduled', (e: Campaign) =>
