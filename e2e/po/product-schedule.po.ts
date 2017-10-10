ac6e2952a262fec23dc97bc122bfae02ece74ad6
Nikola Bosic <nikola.bosic@coingcs.com>
2017-10-10 16:35:40 +0200

    Add e2e tests for product schedule

---
+++ e2e/po/product-schedule.po.ts
@@ -0,0 +1,56 @@
+import {element, by} from 'protractor';
+
+export class ProductSchedulePage {
+
+  getNewProductScheduleForm() {
+    return element(by.css('product-schedule-add-new'));
+  }
+
+  getNewProductScheduleInputs() {
+    return element(by.css('product-schedule-add-new')).all(by.css('input'));
+  }
+
+  getNewProductScheduleSaveButton() {
+    return element(by.css('.entity-view__card__actions')).all(by.css('div')).last();
+  }
+
+  getErrorInputs() {
+    return element(by.css('product-schedule-add-new')).all(by.css('.ng-invalid'));
+  }
+
+  getProductScheduleName() {
+    return element(by.css('.entity-view__info__data__name'));
+  }
+
+  getDetailsMenuButton() {
+    return element(by.css('.entity-view__card__header')).element(by.css('md-icon'));
+  }
+
+  getMenuButton(index: number) {
+    return element(by.css('.md-menu-content')).all(by.css('button')).get(index);
+  }
+
+  getAutoCompleteOption() {
+    return element(by.css('.default-autocomplete-input__options__item'));
+  }
+
+  getAddScheduleInputs() {
+    return element(by.css('add-schedule')).all(by.css('input'));
+  }
+
+  getAddScheduleAddButton() {
+    return element(by.css('add-schedule')).all(by.css('.entity-view__card__actions')).all(by.css('div')).get(1);
+  }
+
+  getAssociatedSchedulesRows() {
+    return element(by.css('tbody')).all(by.css('tr'));
+  }
+
+  getTableRowOptionsButton() {
+    return element(by.css('tbody')).element(by.css('tr')).element(by.css('md-icon'));
+  }
+
+  getConfirmDeleteButton() {
+    return element(by.css('delete-dialog')).element(by.css('md-card-actions')).all(by.css('div')).last();
+  }
+}
---
+++ e2e/specs/product-schedule.e2e-spec.ts
@@ -0,0 +1,120 @@
+import {waitForUrlContains} from '../utils/navigation.utils';
+import {EntityIndexPage} from '../po/entity-index.po';
+import {SidenavPage} from '../po/sidenav.po';
+import {login} from '../utils/action.utils';
+import {browser} from 'protractor';
+import {expectUrlToContain, expectDefined} from '../utils/assertation.utils';
+import {ProductSchedulePage} from '../po/product-schedule.po';
+
+describe('Product Schedules', function() {
+  let page: EntityIndexPage;
+  let productSchedulePage: ProductSchedulePage;
+
+  beforeEach(() => {
+    page = new EntityIndexPage();
+    productSchedulePage = new ProductSchedulePage();
+  });
+
+  beforeAll(() => {
+    browser.driver.manage().window().setSize(1440, 900);
+
+    login();
+    waitForUrlContains('dashboard');
+  });
+
+  it('should navigate to products page', () => {
+    const sidenav = new SidenavPage();
+    sidenav.getLink(9).click();
+    browser.sleep(500);
+    sidenav.getLink(11).click();
+    waitForUrlContains('productschedules');
+    expectUrlToContain('productschedules');
+  });
+
+  it('should render product schedules index component', () => {
+    expectDefined(page.getComponent());
+  });
+
+  it('should render product schedules index title', () => {
+    expect(page.getTitle().getText()).toContain('Product Schedules')
+  });
+
+  it('should render product schedules index add button', () => {
+    expectDefined(page.getAddButton());
+  });
+
+  it('should render product schedules index table headers', () => {
+    expect(page.getTableHeaders().get(0).getText()).toEqual('ID');
+    expect(page.getTableHeaders().get(1).getText()).toEqual('Name');
+    expect(page.getTableHeaders().get(2).getText()).toEqual('Number Of Product Schedules');
+  });
+
+  it('should render add modal when add button is clicked', () => {
+    page.getAddButton().click();
+    expectDefined(productSchedulePage.getNewProductScheduleForm());
+  });
+
+  it('should show show errors when try to submit empty form', () => {
+    productSchedulePage.getNewProductScheduleSaveButton().click();
+    expect(productSchedulePage.getErrorInputs().count()).toBeGreaterThan(1);
+  });
+
+  it('should remove errors when form is valid', () => {
+    productSchedulePage.getNewProductScheduleInputs().get(0).sendKeys('e2e product schedule');
+    productSchedulePage.getNewProductScheduleInputs().get(1).click();
+    browser.sleep(1000);
+    productSchedulePage.getAutoCompleteOption().click();
+    expect(productSchedulePage.getErrorInputs().count()).toEqual(0);
+  });
+
+  it('should create new product schedule and redirect product schedule view', () => {
+    productSchedulePage.getNewProductScheduleSaveButton().click();
+    waitForUrlContains('productschedules/');
+    expectUrlToContain('productschedules/');
+  });
+
+  it('should display product schedule details', () => {
+    browser.sleep(2000);
+    expect(productSchedulePage.getProductScheduleName().getText()).toEqual('e2e product schedule');
+    expect(productSchedulePage.getAssociatedSchedulesRows().count()).toEqual(0);
+  });
+
+  it('should update product schedules', () => {
+    productSchedulePage.getDetailsMenuButton().click();
+    browser.sleep(500);
+    productSchedulePage.getMenuButton(0).click();
+    browser.sleep(200);
+
+    productSchedulePage.getNewProductScheduleInputs().get(0).sendKeys(' updated');
+    productSchedulePage.getNewProductScheduleSaveButton().click();
+  });
+
+  it('should persist updated product schedule details', () => {
+    browser.sleep(2000);
+    expect(productSchedulePage.getProductScheduleName().getText()).toEqual('e2e product schedule updated');
+  });
+
+  it('should add new schedule', () => {
+    productSchedulePage.getAddScheduleInputs().get(0).click();
+    browser.sleep(500);
+    productSchedulePage.getAutoCompleteOption().click();
+    browser.sleep(500);
+    productSchedulePage.getAddScheduleAddButton().click();
+    browser.sleep(2000);
+
+    expect(productSchedulePage.getAssociatedSchedulesRows().count()).toEqual(1);
+  });
+
+  it('should remove added schedule', () => {
+    productSchedulePage.getTableRowOptionsButton().click();
+    browser.sleep(500);
+
+    productSchedulePage.getMenuButton(1).click();
+    browser.sleep(500);
+    productSchedulePage.getConfirmDeleteButton().click();
+    browser.sleep(2000);
+
+    expect(productSchedulePage.getAssociatedSchedulesRows().count()).toEqual(0);
+  })
+
+});
