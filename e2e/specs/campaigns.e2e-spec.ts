f6bce7fce5a2983400c8b4605b7f18508c1c3906
Nikola Bosic <nikola.bosic@coingcs.com>
2017-10-13 16:45:50 +0200

    Add e2e test for campaign index

---
+++ e2e/specs/campaigns.e2e-spec.ts
@@ -0,0 +1,53 @@
+import {waitForUrlContains} from '../utils/navigation.utils';
+import {EntityIndexPage} from '../po/entity-index.po';
+import {SidenavPage} from '../po/sidenav.po';
+import {login} from '../utils/action.utils';
+import {browser} from 'protractor';
+import {expectUrlToContain, expectDefined} from '../utils/assertation.utils';
+
+describe('Campaigns', function() {
+  let page: EntityIndexPage;
+
+  beforeEach(() => {
+    page = new EntityIndexPage();
+  });
+
+  beforeAll(() => {
+    browser.driver.manage().window().setSize(1440, 900);
+
+    login();
+    waitForUrlContains('dashboard');
+  });
+
+  it('should navigate to products page', () => {
+    const sidenav = new SidenavPage();
+    sidenav.getLink(9).click();
+    browser.sleep(500);
+    sidenav.getLink(12).click();
+    waitForUrlContains('campaigns');
+    expectUrlToContain('campaigns');
+  });
+
+  it('should render campaigns index component', () => {
+    expectDefined(page.getComponent());
+  });
+
+  it('should render campaigns index title', () => {
+    expect(page.getTitle().getText()).toContain('Campaigns')
+  });
+
+  it('should render campaigns index add button', () => {
+    expectDefined(page.getAddButton());
+  });
+
+  it('should render campaigns index table headers', () => {
+    expect(page.getTableHeaders().get(0).getText()).toEqual('Name');
+    expect(page.getTableHeaders().get(1).getText()).toEqual('Total products');
+    expect(page.getTableHeaders().get(2).getText()).toEqual('Total Scheduled');
+    expect(page.getTableHeaders().get(3).getText()).toEqual('Created at');
+  });
+
+  it('should render add modal when add button is clicked', () => {
+    page.getAddButton().click();
+  });
+});
