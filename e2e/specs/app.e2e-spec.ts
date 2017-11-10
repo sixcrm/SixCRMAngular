4f9cc16baeeecb603ef114077265d06f36e3a3a4
Nikola Bosic <nikola.bosic@coingcs.com>
2017-11-10 14:17:34 +0100

    Fix app e2e tests

--- e2e/specs/app.e2e-spec.ts
+++ e2e/specs/app.e2e-spec.ts
@@ -1,15 +1,18 @@
 import { AuthPage } from '../po/auth.po';
 import {expectPresent} from '../utils/assertation.utils';
 import {clearLocalStorage} from '../utils/navigation.utils';
+import {browser} from 'protractor';
 
 describe('App load', function() {
   let authPage: AuthPage;
 
   beforeEach(() => {
     authPage = new AuthPage();
+    browser.waitForAngularEnabled(false);
   });
 
   afterEach(() => {
+    browser.waitForAngularEnabled(true);
     clearLocalStorage();
   });
 
