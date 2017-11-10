61fac762f10896674089407c056c3ee60b0c463c
Nikola Bosic <nikola.bosic@coingcs.com>
2017-11-10 14:51:29 +0100

    Fix navigation e2e test

--- e2e/specs/navigation.e2e-spec.ts
+++ e2e/specs/navigation.e2e-spec.ts
@@ -28,7 +28,7 @@ describe('Navigation', function() {
     waitForUrlContains('/dashboard');
 
     browser.sleep(100);
-    expect(sidenav.getItems().count()).toBe(34);
+    expect(sidenav.getItems().count()).toBe(36);
   });
 
   it('should navigate to dashboard', () => {
@@ -39,150 +39,150 @@ describe('Navigation', function() {
   });
 
   it('should navigate to customers', () => {
-    sidenav.getLink(8).click();
+    sidenav.getLink(10).click();
 
     waitForUrlContains('customers');
     expectUrlToContain('customers');
   });
 
   it('should navigate to products', () => {
-    sidenav.getLink(9).click();
+    sidenav.getLink(11).click();
     browser.sleep(500);
-    sidenav.getLink(10).click();
+    sidenav.getLink(12).click();
 
     waitForUrlContains('products');
     expectUrlToContain('products');
   });
 
   it('should navigate to product schedules', () => {
-    sidenav.getLink(11).click();
+    sidenav.getLink(13).click();
 
     waitForUrlContains('productschedules');
     expectUrlToContain('productschedules');
   });
 
   it('should navigate to campaigns', () => {
-    sidenav.getLink(12).click();
+    sidenav.getLink(14).click();
 
     waitForUrlContains('campaigns');
     expectUrlToContain('campaigns');
   });
 
   it('should navigate to email templates', () => {
-    sidenav.getLink(13).click();
+    sidenav.getLink(15).click();
 
     waitForUrlContains('emailtemplates');
     expectUrlToContain('emailtemplates');
   });
 
   it('should navigate to affiliates', () => {
-    sidenav.getLink(14).click();
+    sidenav.getLink(16).click();
 
     waitForUrlContains('affiliates');
     expectUrlToContain('affiliates');
   });
 
   it('should navigate to trackers', () => {
-    sidenav.getLink(15).click();
+    sidenav.getLink(17).click();
 
     waitForUrlContains('trackers');
     expectUrlToContain('trackers');
   });
 
   it('should navigate to sessions', () => {
-    sidenav.getLink(16).click();
+    sidenav.getLink(18).click();
 
     waitForUrlContains('sessions');
     expectUrlToContain('sessions');
   });
 
   it('should navigate to transactions', () => {
-    sidenav.getLink(17).click();
+    sidenav.getLink(19).click();
 
     waitForUrlContains('transactions');
     expectUrlToContain('transactions');
   });
 
   it('should navigate to rebills', () => {
-    sidenav.getLink(18).click();
+    sidenav.getLink(20).click();
 
     waitForUrlContains('rebills');
     expectUrlToContain('rebills');
   });
 
   it('should navigate to fulfillment providers', () => {
-    sidenav.getLink(19).click();
+    sidenav.getLink(21).click();
     browser.sleep(500);
-    sidenav.getLink(20).click();
+    sidenav.getLink(22).click();
 
     waitForUrlContains('fulfillmentproviders');
     expectUrlToContain('fulfillmentproviders');
   });
 
   it('should navigate to smtp providers', () => {
-    sidenav.getLink(21).click();
+    sidenav.getLink(23).click();
 
     waitForUrlContains('smtpproviders');
     expectUrlToContain('smtpproviders');
   });
 
   it('should navigate to merchant providers', () => {
-    sidenav.getLink(22).click();
+    sidenav.getLink(24).click();
     browser.sleep(500);
-    sidenav.getLink(23).click();
+    sidenav.getLink(25).click();
 
     waitForUrlContains('merchantproviders');
     expectUrlToContain('merchantproviders');
   });
 
   it('should navigate to loadbalancers', () => {
-    sidenav.getLink(24).click();
+    sidenav.getLink(26).click();
 
     waitForUrlContains('loadbalancers');
     expectUrlToContain('loadbalancers');
   });
 
   it('should navigate to users', () => {
-    sidenav.getLink(25).click();
+    sidenav.getLink(27).click();
     browser.sleep(500);
-    sidenav.getLink(26).click();
+    sidenav.getLink(28).click();
 
     waitForUrlContains('users');
     expectUrlToContain('users');
   });
 
   it('should navigate to accounts', () => {
-    sidenav.getLink(27).click();
+    sidenav.getLink(29).click();
 
     waitForUrlContains('accounts');
     expectUrlToContain('accounts');
   });
 
   it('should navigate to billing', () => {
-    sidenav.getLink(28).click();
+    sidenav.getLink(30).click();
 
     waitForUrlContains('billing');
     expectUrlToContain('billing');
   });
 
   it('should navigate to roles', () => {
-    sidenav.getLink(29).click();
+    sidenav.getLink(31).click();
 
     waitForUrlContains('roles');
     expectUrlToContain('roles');
   });
 
   it('should navigate to graph docs', () => {
-    sidenav.getLink(30).click();
+    sidenav.getLink(32).click();
     browser.sleep(500);
-    sidenav.getLink(31).click();
+    sidenav.getLink(33).click();
 
     waitForUrlContains('documentation/graph');
     expectUrlToContain('documentation/graph');
   });
 
   it('should navigate to search', () => {
-    sidenav.getLink(33).click();
+    sidenav.getLink(35).click();
 
     waitForUrlContains('search');
     expectUrlToContain('search');
