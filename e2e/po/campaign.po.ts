9256268aa94989f439ad35cf86cae3b409da59f8
Nikola Bosic <nikola.bosic@coingcs.com>
2017-11-20 16:39:36 +0100

    Add campaign e2e tests

---
+++ e2e/po/campaign.po.ts
@@ -0,0 +1,41 @@
+import {element, by} from 'protractor';
+
+export class CampaignPage {
+
+  getNewCampaignForm() {
+    return element(by.css('campaign-add-new'));
+  }
+
+  getNewCampaignFormNameInput() {
+    return element(by.css('campaign-add-new')).element(by.css('input'));
+  }
+
+  getNewCampaignFormInvalidInputs() {
+    return element(by.css('campaign-add-new')).all(by.css('.ng-invalid'));
+  }
+
+  getCampaignFormCheckboxes() {
+    return element(by.css('campaign-add-new')).element(by.css('md-checkbox'));
+  }
+
+  getCampaignFormSaveButton() {
+    return element(by.css('.entity-view__card__actions')).all(by.css('div')).last();
+  }
+
+  getCampaignNameInHeader() {
+    return element(by.css('.entity-view__info__data__name'));
+  }
+
+  getMenuButton() {
+    return element(by.css('.entity-view__card__header')).element(by.css('md-icon'));
+  }
+
+  getEditButton() {
+    return element(by.css('.md-menu-content')).element(by.css('button'));
+  }
+
+  getCampaignNameInCard() {
+    return element(by.css('.entity-view__card__content')).element(by.css('input'));
+  }
+
+}
--- e2e/specs/campaigns.e2e-spec.ts
+++ e2e/specs/campaigns.e2e-spec.ts
@@ -4,12 +4,15 @@ import {SidenavPage} from '../po/sidenav.po';
 import {login} from '../utils/action.utils';
 import {browser} from 'protractor';
 import {expectUrlToContain, expectDefined} from '../utils/assertation.utils';
+import {CampaignPage} from '../po/campaign.po';
 
-describe('Campaigns', function() {
+fdescribe('Campaigns', function() {
   let page: EntityIndexPage;
+  let campaignPage: CampaignPage;
 
   beforeEach(() => {
     page = new EntityIndexPage();
+    campaignPage = new CampaignPage();
   });
 
   beforeAll(() => {
@@ -51,5 +54,42 @@ describe('Campaigns', function() {
 
   it('should render add modal when add button is clicked', () => {
     page.getAddButton().click();
+
+    expectDefined(campaignPage.getNewCampaignForm());
   });
+
+  it('should show error when try to save without name', () => {
+    campaignPage.getCampaignFormSaveButton().click();
+
+    expect(campaignPage.getNewCampaignFormInvalidInputs().count()).toBeGreaterThan(0);
+  });
+
+  it('should remove error when proper name is entered', () => {
+    campaignPage.getNewCampaignFormNameInput().sendKeys('e2e test campaign');
+
+    expect(campaignPage.getNewCampaignFormInvalidInputs().count()).toBe(0);
+  });
+
+  it('should save campaign and open it', () => {
+    campaignPage.getCampaignFormSaveButton().click();
+
+    waitForUrlContains('campaigns/');
+    expectUrlToContain('campaigns/');
+  });
+
+  it('should render campaign name correctly', () => {
+    expect(campaignPage.getCampaignNameInHeader().getText()).toEqual('e2e test campaign');
+  });
+
+  it('should update campaign name', () => {
+    campaignPage.getMenuButton().click();
+    browser.sleep(200);
+    campaignPage.getEditButton().click();
+    browser.sleep(200);
+    campaignPage.getCampaignNameInCard().sendKeys(' updated');
+    campaignPage.getCampaignFormSaveButton();
+
+    browser.sleep(2000);
+    expect(campaignPage.getCampaignNameInHeader().getText()).toEqual('e2e test campaign updated');
+  })
 });
