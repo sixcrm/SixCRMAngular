17d6a6f991c4c2b758befe529c9951761631152e
Nikola B <nikola.bosic@toptal.com>
2017-11-27 13:58:23 +0100

    Update pixel tracking js code

--- src/app/pages/campaigns-page/campaign-view/pixel-tracker/pixel-tracker.component.ts
+++ src/app/pages/campaigns-page/campaign-view/pixel-tracker/pixel-tracker.component.ts
@@ -17,8 +17,6 @@ export class PixelTrackerComponent implements OnInit, OnDestroy {
   @Input() campaignId: string;
   accountId: string;
   endpoint: string = environment.bareEndpoint;
-  key: string = '1ud98uhc9h989811ud01yd81u2d1289duu1du1a0d9uula';
-  secretKey: string = 'c3VwZXIudXNlckB0ZXN0LmNvbQ==';
 
   scriptContent: string;
 
@@ -36,7 +34,7 @@ export class PixelTrackerComponent implements OnInit, OnDestroy {
   constructor(private authService: AuthenticationService) { }
 
   ngOnInit() {
-    this.accountId = `"${this.authService.getActiveAcl().account.id}"`;
+    this.accountId = `${this.authService.getActiveAcl().account.id}`;
     this.scriptContent = `<script>\nfunction getAffiliateId(){var e=new RegExp("[\\?&]affiliate_id=([^&#]*)").exec(location.href),t=document.head.querySelector("meta[name='affiliate_id']"),i=("; "+document.cookie).split("; affiliate_id="),a=2===i.length?i.pop().split(";").shift():"",n=e?e[1]:t?t.content:affiliateId||a,o=new Date;return o.setTime(o.getTime()+864e5),document.cookie="affiliate_id="+n+"; expires="+o.toUTCString()+"; path=/",n}function getTrackers(e){var t=new XMLHttpRequest;t.open("POST","${this.endpoint}tracking/${this.accountId}",!0),t.setRequestHeader("Authorization",e),t.onreadystatechange=function(){if(4===t.readyState&&200===t.status){var e=JSON.parse(t.responseText).response.trackers;if(e&&e.length>0)for(var i,a=0;a<e.length;a++)(i=document.createElement("div")).innerHTML=e[a].body,document.body.appendChild(i)}},t.send('{"campaign":"${this.campaignId}","affiliate_id":"'+getAffiliateId()+'"}')}\n</script>`;
     this.showScript = true;
 
--- src/app/pages/state-machine-page/state-machine-dashboard/state-machine-details/state-machine-details.component.ts
+++ src/app/pages/state-machine-page/state-machine-dashboard/state-machine-details/state-machine-details.component.ts
@@ -1,7 +1,6 @@
 import {Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
 import {StateMachineTimeseries} from '../../../../shared/models/state-machine/state-machine-timeseries.model';
 import {StateMachineQueue} from '../../../../shared/models/state-machine/state-machine-queue';
-import {DateMap} from '../../../../shared/components/advanced-filter/advanced-filter.component';
 
 @Component({
   selector: 'state-machine-details',
@@ -14,10 +13,10 @@ export class StateMachineDetailsComponent implements OnInit {
   @Input() standalone: boolean;
   @Input() isPolling: boolean;
   @Input() timeseries: StateMachineTimeseries[];
-  @Input() date: DateMap;
+  @Input() date: any;
 
   @Output() togglePolling: EventEmitter<boolean> = new EventEmitter();
-  @Output() dateChanged: EventEmitter<DateMap> = new EventEmitter();
+  @Output() dateChanged: EventEmitter<any> = new EventEmitter();
 
   constructor() { }
 
