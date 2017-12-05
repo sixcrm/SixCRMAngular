d5eef2d6437e7bba3f8bc7282d89f6e9b950e774
Nikola Bosic <nikola.bosic@coingcs.com>
2017-12-05 13:57:23 +0100

    Update empty charts on details pages

--- src/app/charts/state-machine-details-chart/state-machine-details-chart.component.ts
+++ src/app/charts/state-machine-details-chart/state-machine-details-chart.component.ts
@@ -9,17 +9,23 @@ import {StateMachineTimeseries} from '../../shared/models/state-machine/state-ma
 export class StateMachineDetailsChartComponent implements OnInit {
 
   chartInstance;
+  chartEmptyInstance;
   stateMachineTimeseries: StateMachineTimeseries[] = [];
+  showNoData: boolean;
 
   @Input() set timeseries(timeseries: StateMachineTimeseries[]) {
     if (timeseries) {
       this.stateMachineTimeseries = timeseries;
     }
 
-    if (this.chartInstance) {
+    this.showNoData = this.stateMachineTimeseries.length === 0;
+
+    if (this.chartInstance && this.chartEmptyInstance) {
       this.redrawChart();
     }
   }
+
+  @Input() date: any;
 
   chartOptions = {
     title: {
@@ -27,6 +33,26 @@ export class StateMachineDetailsChartComponent implements OnInit {
     },
     credits: {
       enabled: false
+    },
+    yAxis: {
+      title: {
+        enabled: false
+      }
+    },
+    series: [
+      { showInLegend: false, name: 'count', color: '#F28933' },
+    ]
+  };
+
+  chartEmptyOptions = {
+    title: {
+      text: null
+    },
+    credits: {
+      enabled: false
+    },
+    xAxis: {
+      type: 'datetime'
     },
     yAxis: {
       title: {
@@ -49,13 +75,36 @@ export class StateMachineDetailsChartComponent implements OnInit {
     this.redrawChart();
   }
 
+  saveEmptyChart(chartInstance): void {
+    this.chartEmptyInstance = chartInstance;
+
+    this.redrawChart();
+  }
+
   redrawChart() {
     if (!this.stateMachineTimeseries) return;
 
+    if (this.stateMachineTimeseries.length > 0) {
+      this.updateChart();
+    } else {
+      this.updateEmptyChart();
+    }
+  }
+
+  updateChart() {
+    if (!this.chartEmptyInstance) return;
+
     const values = this.stateMachineTimeseries.map(s => s.count);
-    const dates = this.stateMachineTimeseries.map(s => s.datetime.clone().format('MM/DD'));
+    const dates = this.stateMachineTimeseries.map(s => s.datetime.clone().format('DD.[ ]MMM'));
     this.chartInstance.xAxis[0].update({categories: dates}, true);
     this.chartInstance.series[0].setData(values, true);
+  }
+
+  updateEmptyChart() {
+    if (!this.chartEmptyInstance) return;
+
+    this.chartEmptyInstance.xAxis[0].update({min: this.date.start.clone().valueOf(), max: this.date.end.clone().valueOf()}, true);
+    this.chartEmptyInstance.yAxis[0].update({min: 0, max: 1000}, true);
   }
 
 }
