23e761468cdfd4517757cbc3dedca45c52521ca5
Nikola Bosic <nikola.bosic@coingcs.com>
2017-12-26 13:21:55 +0100

    Update user e2e tests

--- e2e/specs/user.e2e-spec.ts
+++ e2e/specs/user.e2e-spec.ts
@@ -1,10 +1,9 @@
-import {waitForUrlContains} from '../utils/navigation.utils';
+import {waitForUrlContains, navigateSuperuserToHomepage} from '../utils/navigation.utils';
 import {EntityIndexPage} from '../po/entity-index.po';
 import {SidenavPage} from '../po/sidenav.po';
 import {browser} from 'protractor';
-import {expectUrlToContain, expectDefined, expectNotPresent} from '../utils/assertation.utils';
+import {expectUrlToContain, expectDefined, expectPresent} from '../utils/assertation.utils';
 import {EntityViewPage} from '../po/entity-view.po';
-import {login} from '../utils/action.utils';
 
 describe('Users', function() {
   let page: EntityIndexPage;
@@ -20,15 +19,16 @@ describe('Users', function() {
     browser.driver.manage().window().setSize(1440, 900);
 
     browser.get('/');
-    login();
+    browser.waitForAngularEnabled(false);
+    navigateSuperuserToHomepage();
     waitForUrlContains('dashboard');
   });
 
   it('should navigate to users page', () => {
     const sidenav = new SidenavPage();
-    sidenav.getLink(28).click();
-    browser.sleep(500);
     sidenav.getLink(29).click();
+    browser.sleep(500);
+    sidenav.getLink(30).click();
     waitForUrlContains('user');
     expectUrlToContain('user');
   });
@@ -41,8 +41,8 @@ describe('Users', function() {
     expect(page.getTitle().getText()).toContain('Users')
   });
 
-  it('should not render users index add button', () => {
-    expectNotPresent(page.getAddButton());
+  it('should render users index add button', () => {
+    expectPresent(page.getAddButton());
   });
 
   it('should render users index table headers', () => {
