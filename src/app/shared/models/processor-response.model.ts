a82e6b40c8519e0f2211130c149f8edab3983d29
Nikola Bosic <nikola.bosic@coingcs.com>
2018-02-01 16:50:14 +0100

    Update processor response model

--- src/app/shared/models/processor-response.model.ts
+++ src/app/shared/models/processor-response.model.ts
@@ -1,21 +1,24 @@
 import {Entity} from './entity.interface';
 
 export class ProcessorResponse implements Entity<ProcessorResponse> {
-  id: string = '';
-  message: string = '';
-  authCode: string = '';
-  responseText: string = '';
-  transactionId: string = '';
+  id: string;
+  message: string;
+  code: string;
+  authCode: string;
+  responseText: string;
+  transactionId: string;
 
   constructor(obj?: any) {
     if (obj) {
       obj = JSON.parse(obj);
-      this.message = obj.message || '';
 
-      if (obj.results) {
-        this.responseText = obj.results.responsetext || '';
-        this.authCode = obj.results.authcode || '';
-        this.transactionId = obj.results.transactionid || '';
+      this.message = obj.message || '';
+      this.code = obj.code || '';
+
+      if (obj.result) {
+        this.responseText = obj.result.responsetext || '';
+        this.authCode = obj.result.authcode || '';
+        this.transactionId = obj.result.transactionid || '';
       }
     }
   }
@@ -28,7 +31,8 @@ export class ProcessorResponse implements Entity<ProcessorResponse> {
     return JSON.stringify({
       id: this.id,
       message: this.message,
-      results: {
+      code: this.code,
+      result: {
         responsetext: this.responseText,
         authcode: this.authCode,
         transactionid: this.transactionId
