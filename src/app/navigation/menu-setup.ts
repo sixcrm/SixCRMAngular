f85555e006d13dc2e81e0d1f44ba928d4a12a30f
Nikola Bosic <nikola.bosic@coingcs.com>
2018-02-14 14:45:17 +0100

    Reorder left navbar

--- src/app/navigation/menu-setup.ts
+++ src/app/navigation/menu-setup.ts
@@ -16,61 +16,72 @@ export function menuItems(authService: AuthenticationService, acl: Acl): MenuIte
     items.push(new MenuItem('SIDENAV_ORDERENGINE', 'state-machine').setIcon('device_hub'));
   }
 
+  // Add Order items
+  let orderItems: MenuItem[] = [];
+
+  if (authService.hasPermissions('customer', 'view')) {
+    orderItems.push(new MenuItem('SIDENAV_ORDER_CUSTOMER', 'customers'));
+  }
+  if (authService.hasPermissions('creditcard', 'view')) {
+    orderItems.push(new MenuItem('SIDENAV_ORDER_CREDITCARD', 'creditcards'));
+  }
+  if (authService.hasPermissions('session', 'view')) {
+    orderItems.push(new MenuItem('SIDENAV_ORDER_SESSION', 'sessions'));
+  }
+  if (authService.hasPermissions('transaction', 'view')) {
+    orderItems.push(new MenuItem('SIDENAV_ORDER_TRANSACTION', 'transactions'));
+  }
+  if (authService.hasPermissions('rebill', 'view')) {
+    orderItems.push(new MenuItem('SIDENAV_ORDER_REBILL', 'rebills'));
+  }
+  if (authService.hasPermissions('shippingreceipt', 'view')) {
+    orderItems.push(new MenuItem('SIDENAV_ORDER_SHIPPINGRECEIPT', 'shippingreceipts'));
+  }
+
+  if (orderItems.length > 0) {
+    items.push(new MenuItem('SIDENAV_ORDER_TITLE', null, orderItems).setIcon('payment'));
+  }
+
   // Add reports
   let reportItems: MenuItem[] = [];
   if (authService.hasPermissions('analytics', 'getOrders')) {
     reportItems.push(new MenuItem('SIDENAV_REPORTS_ORDER', 'reports/order'));
   }
-
   if (authService.hasPermissions('analytics', 'getTransactions')) {
     reportItems.push(new MenuItem('SIDENAV_REPORTS_SUMMARY', 'reports/summary'));
   }
-
   if (authService.hasPermissions('analytics', 'getTransactions')) {
     reportItems.push(new MenuItem('SIDENAV_REPORTS_TRANSACTION', 'reports/transaction'));
   }
-
   if (authService.hasPermissions('analytics', 'getMerchants')) {
     reportItems.push(new MenuItem('SIDENAV_REPORTS_MERCHANT', 'reports/merchant'));
   }
-
   if (authService.hasPermissions('analytics', 'getAffiliates')) {
     reportItems.push(new MenuItem('SIDENAV_REPORTS_AFFILIATE', 'reports/affiliate'));
   }
-
   if (authService.hasPermissions('analytics', 'getFullfillment')) {
     reportItems.push(new MenuItem('SIDENAV_REPORTS_FULFILLMENT', 'reports/fulfillment'));
   }
-
   if (authService.hasPermissions('analytics', 'getRetention')) {
     reportItems.push(new MenuItem('SIDENAV_REPORTS_RETENTION', 'reports/retention'));
   }
-
   if (authService.hasPermissions('analytics', 'getProjections')) {
     reportItems.push(new MenuItem('SIDENAV_REPORTS_PROJECTION', 'reports/projection'));
   }
-
   if (reportItems.length > 0) {
     items.push(new MenuItem('SIDENAV_REPORTS_TITLE', null, reportItems).setIcon('library_books'));
   }
 
-  // Add customer menu items
-  items.push(new MenuItem('SIDENAV_CUSTOMER', 'customers').setIcon('account_box'));
-
-  if (authService.hasPermissions('creditcard', 'view')) {
-    items.push(new MenuItem('SIDENAV_CREDITCARD', 'creditcards').setIcon('payment'));
-  }
-
   // Add CRM menu items
   let crmItems: MenuItem[] = [];
+  if (authService.hasPermissions('campaign', 'view')) {
+    crmItems.push(new MenuItem('SIDENAV_CRM_CAMPAIGN', 'campaigns'));
+  }
   if (authService.hasPermissions('product', 'view')) {
     crmItems.push(new MenuItem('SIDENAV_CRM_PRODUCT', 'products'));
   }
   if (authService.hasPermissions('productschedule', 'view')) {
     crmItems.push(new MenuItem('SIDENAV_CRM_PRODUCTSCHEDULE', 'productschedules'));
-  }
-  if (authService.hasPermissions('campaign', 'view')) {
-    crmItems.push(new MenuItem('SIDENAV_CRM_CAMPAIGN', 'campaigns'));
   }
   if (authService.hasPermissions('emailtemplate', 'view')) {
     crmItems.push(new MenuItem('SIDENAV_CRM_EMAILTEMPLATE', 'emailtemplates'));
@@ -81,17 +92,17 @@ export function menuItems(authService: AuthenticationService, acl: Acl): MenuIte
   if (authService.hasPermissions('tracker', 'view')) {
     crmItems.push(new MenuItem('SIDENAV_CRM_TRACKER', 'trackers'));
   }
-  if (authService.hasPermissions('session', 'view')) {
-    crmItems.push(new MenuItem('SIDENAV_CRM_SESSION', 'sessions'));
+
+  // Add Merchants menu item
+  let merchants: MenuItem[] = [];
+  if (authService.hasPermissions('merchantprovider', 'view')) {
+    merchants.push(new MenuItem('SIDENAV_CRM_MERCHANT_MERCHANTPROVIDER', 'merchantproviders'))
   }
-  if (authService.hasPermissions('transaction', 'view')) {
-    crmItems.push(new MenuItem('SIDENAV_CRM_TRANSACTION', 'transactions'));
+  if (authService.hasPermissions('loadbalancer', 'view')) {
+    merchants.push(new MenuItem('SIDENAV_CRM_MERCHANT_LOADBALANCER', 'merchantprovidergroups'))
   }
-  if (authService.hasPermissions('rebill', 'view')) {
-    crmItems.push(new MenuItem('SIDENAV_CRM_REBILL', 'rebills'));
-  }
-  if (authService.hasPermissions('shippingreceipt', 'view')) {
-    crmItems.push(new MenuItem('SIDENAV_CRM_SHIPPINGRECEIPT', 'shippingreceipts'));
+  if (merchants.length > 0) {
+    crmItems.push(new MenuItem('SIDENAV_CRM_MERCHANT_TITLE', null, merchants));
   }
 
   // Add 3rd party providers to CRM menu
@@ -110,24 +121,8 @@ export function menuItems(authService: AuthenticationService, acl: Acl): MenuIte
     items.push(new MenuItem('SIDENAV_CRM_TITLE', null, crmItems).setIcon('account_balance'));
   }
 
-  // Add Merchants menu item
-  let merchants: MenuItem[] = [];
-  if (authService.hasPermissions('merchantprovider', 'view')) {
-    merchants.push(new MenuItem('SIDENAV_MERCHANT_MERCHANTPROVIDER', 'merchantproviders'))
-  }
-  if (authService.hasPermissions('loadbalancer', 'view')) {
-    merchants.push(new MenuItem('SIDENAV_MERCHANT_LOADBALANCER', 'merchantprovidergroups'))
-  }
-
-  if (merchants.length > 0) {
-    items.push(new MenuItem('SIDENAV_MERCHANT_TITLE', null, merchants).setIcon('payment'));
-  }
-
   // Add Settings menu item
   let settings: MenuItem[] = [];
-  if (authService.hasPermissions('user', 'view') && authService.isActiveAclMasterAccount()) {
-    settings.push(new MenuItem('SIDENAV_SETTINGS_USER', 'users'));
-  }
   if (authService.hasPermissions('account', 'view')) {
     let accountlink = 'accounts';
 
@@ -137,11 +132,15 @@ export function menuItems(authService: AuthenticationService, acl: Acl): MenuIte
 
     settings.push(new MenuItem('SIDENAV_SETTINGS_ACCOUNT', accountlink));
   }
-  if (authService.hasPermissions('bill', 'view') && authService.isActiveAclMasterAccount()) {
-    settings.push(new MenuItem('SIDENAV_SETTINGS_BILL', 'bills'));
+  if (authService.hasPermissions('user', 'view') && authService.isActiveAclMasterAccount()) {
+    settings.push(new MenuItem('SIDENAV_SETTINGS_USER', 'users'));
   }
   if (authService.hasPermissions('role', 'view')) {
     settings.push(new MenuItem('SIDENAV_SETTINGS_ROLE', 'roles'));
+  }
+
+  if (authService.hasPermissions('bill', 'view') && authService.isActiveAclMasterAccount()) {
+    settings.push(new MenuItem('SIDENAV_SETTINGS_BILL', 'bills'));
   }
 
   let keys = [];
@@ -157,11 +156,15 @@ export function menuItems(authService: AuthenticationService, acl: Acl): MenuIte
     settings.push(new MenuItem('SIDENAV_SETTINGS_KEY_TITLE', null, keys));
   }
 
-  settings.push(new MenuItem('SIDENAV_SETTINGS_DOCUMENTATION_TITLE', null, [
-    new MenuItem('SIDENAV_SETTINGS_DOCUMENTATION_GRAPHQL', 'documentation/graph'),
-    new MenuItem({title: 'SIDENAV_SETTINGS_DOCUMENTATION_WIKI', clickHandler: () => window.open('https://github.com/sixcrm/SixCRMIntegrations/wiki', '_blank').focus()}, '')
-  ]));
   items.push(new MenuItem('SIDENAV_SETTINGS_TITLE', null, settings).setIcon('settings'));
+
+  // Add Help menu item
+  let help: MenuItem[] = [];
+
+  help.push(new MenuItem('SIDENAV_HELP_GRAPHQL', 'documentation/graph'));
+  help.push(new MenuItem({title: 'SIDENAV_HELP_WIKI', clickHandler: () => window.open('https://github.com/sixcrm/SixCRMIntegrations/wiki', '_blank').focus()}, ''));
+
+  items.push(new MenuItem('SIDENAV_HELP_TITLE', null, help).setIcon('help'));
 
   // Add Search menu item
   if (acl.role.name !== 'Customer Service') {
