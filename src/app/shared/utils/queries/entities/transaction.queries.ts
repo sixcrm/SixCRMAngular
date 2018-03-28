599c7cd672b3aca8ea38e070c75b2675f5c9eac6
Nikola Bosic <nikola.bosic@coingcs.com>
2018-03-28 15:16:55 +0200

    Fix refund query

--- src/app/shared/utils/queries/entities/transaction.queries.ts
+++ src/app/shared/utils/queries/entities/transaction.queries.ts
@@ -44,8 +44,7 @@ export function transactionsByCustomer(customerId: string, limit?:number, cursor
 export function refundTransactionMutation(transactionId: string, refundAmount: string): string {
   return `mutation {
 		refund(refund:{amount:"${refundAmount}", transaction:"${transactionId}"}) {
-			transaction {${transactionResponseQuery()}},
-			processor_response
+			transaction { id }
 		}
 	}`
 }
