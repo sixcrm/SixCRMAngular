7914fcff6df3d7d4da1ac2f04d451f30a61875c6
Ljubomir Simin <ljubomir@coingcs.com>
2018-05-03 18:15:38 +0200

    Fix parsing in merchant provider report.

--- src/app/shared/models/analytics/merchant-report.model.ts
+++ src/app/shared/models/analytics/merchant-report.model.ts
@@ -16,13 +16,26 @@ export class MerchantReport {
       obj = [];
     }
 
-    this.merchantProvider = new MerchantProvider(obj.merchant_provider);
-    this.saleCount = obj.sales || 0;
-    this.saleGrossRevenue = new Currency(obj.sales_revenue);
-    this.refundExpenses = new Currency(obj.total_refund_expenses);
-    this.refundCount = obj.full_refunds || 0 + obj.partial_refunds || 0;
-    this.netRevenue = new Currency(obj.sales_revenue);
-    this.mtdSalesCount = obj.mtd_sales_count || 0;
-    this.mtdGrossCount = obj.mtd_gross_count || 0;
+    this.merchantProvider = new MerchantProvider(MerchantReport.getValueOf('merchant_provider', obj));
+    this.saleCount = MerchantReport.getValueOf('sales', obj) || 0;
+    this.saleGrossRevenue = new Currency(MerchantReport.getValueOf('sales_revenue', obj));
+    this.refundExpenses = new Currency(MerchantReport.getValueOf('total_refund_expenses', obj));
+    this.refundCount = MerchantReport.getValueOf('full_refunds', obj) || 0 + MerchantReport.getValueOf('partial_refunds', obj) || 0;
+    this.netRevenue = new Currency(MerchantReport.getValueOf('sales_revenue', obj));
+    this.mtdSalesCount = MerchantReport.getValueOf('mtd_sales_count', obj) || 0;
+    this.mtdGrossCount = MerchantReport.getValueOf('mtd_gross_count', obj) || 0;
+  }
+
+  public static getValueOf(field, obj) {
+    if (!obj.length) {
+      obj = [];
+    }
+
+    let array = obj.filter(o => o.key === field);
+    if (array.length > 0) {
+      return parseInt(array[0].value)
+    }
+
+    return 0;
   }
 }
