e3aa47138926214f6bc5a707db9bbcd12b6bc427
Nikola Bosic <nikola.bosic@coingcs.com>
2018-05-03 16:31:09 +0200

    Updates on customer service pair component

---
+++ src/app/pages/customer-service-page/customer-service-order/customer-service-return/customer-service-return.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'customer-service-return',
+  templateUrl: './customer-service-return.component.html',
+  styleUrls: ['./customer-service-return.component.scss']
+})
+export class CustomerServiceReturnComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
