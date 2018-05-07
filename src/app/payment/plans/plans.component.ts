3be532f1a1e94ff8fa2f9fb368e246a9907359d6
Nikola Bosic <nikola.bosic@coingcs.com>
2018-05-07 12:09:15 +0200

    Polish plans selection

--- src/app/payment/plans/plans.component.ts
+++ src/app/payment/plans/plans.component.ts
@@ -10,21 +10,32 @@ export class PlansComponent implements OnInit {
 
   @Output() planSelected: EventEmitter<Plan> = new EventEmitter();
 
+  selectedPlan: string;
+
   constructor() { }
 
   ngOnInit() {
   }
 
   selectBasic() {
-    this.planSelected.emit({name: 'Basic', price: 30, id: '37cbb0aa-a1e9-4ad0-afe3-38f1dce31d5b'})
+    this.selectedPlan = 'basic';
+    this.emitPlan({name: 'Basic', price: 30, id: '37cbb0aa-a1e9-4ad0-afe3-38f1dce31d5b'})
   }
 
   selectProfessional() {
-    this.planSelected.emit({name: 'Professional', price: 150, id: '646c2262-95e8-433d-be99-e621f7f17c7b'})
+    this.selectedPlan = 'pro';
+    this.emitPlan({name: 'Professional', price: 150, id: '646c2262-95e8-433d-be99-e621f7f17c7b'})
   }
 
   selectPremium() {
-    this.planSelected.emit({name: 'Premium', price: 2000, id: 'd4dbbe5e-0413-44f1-98fc-a154db812541'})
+    this.selectedPlan = 'premium';
+    this.emitPlan({name: 'Premium', price: 2000, id: 'd4dbbe5e-0413-44f1-98fc-a154db812541'})
+  }
+
+  emitPlan(plan: Plan) {
+    setTimeout(() => {
+      this.planSelected.emit(plan);
+    }, 1000);
   }
 
 }
