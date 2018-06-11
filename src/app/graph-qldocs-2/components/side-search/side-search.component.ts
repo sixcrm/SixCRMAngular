3700c12ae094e2cdc6e449a3cd6997d0db65ac08
Ljubomir Simin <ljubomir@coingcs.com>
2018-06-11 18:11:02 +0200

    Persist state of selected item and search terms.

--- src/app/graph-qldocs-2/components/side-search/side-search.component.ts
+++ src/app/graph-qldocs-2/components/side-search/side-search.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {navigateToFieldByString} from '../../utils';
 import {Router} from "@angular/router";
+import {GraphqlDocs2Service} from "../../graphql-docs-2.service";
 
 export interface SearchItem {
   name: string;
@@ -16,10 +17,10 @@ export class SideSearchComponent implements OnInit {
 
   @Input() searchItems: SearchItem[];
   @Input() focused: boolean = false;
-  filterString: string;
 
   constructor(
-    private router: Router
+    private router: Router,
+    private docsService: GraphqlDocs2Service
   ) { }
 
   ngOnInit() { }
@@ -30,5 +31,21 @@ export class SideSearchComponent implements OnInit {
 
   navigateTo(path: string, type: string): void {
     this.router.navigate(['documentation/graph2/', type.toLowerCase(), path]);
+  }
+
+  get selectedItem(): string {
+    return this.docsService.selectedItem;
+  }
+
+  set selectedItem(item: string) {
+    this.docsService.selectedItem = item;
+  }
+
+  get filterString(): string {
+    return this.docsService.filterString;
+  }
+
+  set filterString(filter: string) {
+    this.docsService.filterString = filter;
   }
 }
