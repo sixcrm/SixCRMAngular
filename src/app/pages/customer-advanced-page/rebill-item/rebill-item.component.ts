5439a38da594480ff6848d2f222e5ba554b2c5af
Nikola Bosic <nikola.bosic@coingcs.com>
2018-06-11 18:54:09 +0200

    Fix build issues

--- src/app/pages/customer-advanced-page/rebill-item/rebill-item.component.ts
+++ src/app/pages/customer-advanced-page/rebill-item/rebill-item.component.ts
@@ -43,7 +43,7 @@ export class RebillItemComponent implements OnInit {
 
     dialogRef.componentInstance.transaction = transaction.copy();
 
-    dialogRef.afterClose.take(1).subscribe(() => {
+    dialogRef.afterClosed.take(1).subscribe(() => {
       dialogRef = null;
     })
   }
