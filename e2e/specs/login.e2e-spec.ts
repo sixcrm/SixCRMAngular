6e6b42ed6d47f8f3c04b26689fab35e79bd640f3
CarlitoEscobar <carlitoescobar@gmail.com>
2018-06-12 10:20:13 -0700

    removed reference to old TOSCheck in login e2e

--- e2e/specs/login.e2e-spec.ts
+++ e2e/specs/login.e2e-spec.ts
@@ -6,7 +6,7 @@ import {
   waitForPresenceOfLoginFields, waitForPresenceOf, waitForUrlContains,
   navigateSuperuserToHomepage, clearLocalStorage
 } from '../utils/navigation.utils';
-import {doLogin, doTOSCheck, tosCheck} from '../utils/action.utils';
+import {doLogin, tosCheck} from '../utils/action.utils';
 import {expectPresent, expectUrlToContain} from '../utils/assertation.utils';
 
 describe('Login', function() {
