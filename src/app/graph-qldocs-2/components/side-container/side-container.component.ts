a2e15c6fc10ac677f8049e6b9b1981b00592e921
Ljubomir Simin <ljubomir@coingcs.com>
2018-06-13 17:01:38 +0200

    Add navigation to type from query/mutation response.

--- src/app/graph-qldocs-2/components/side-container/side-container.component.ts
+++ src/app/graph-qldocs-2/components/side-container/side-container.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'side-container',
@@ -9,9 +10,15 @@ export class SideContainerComponent implements OnInit {
 
   @Input() example: any;
   @Input() response: any;
+  @Input() fieldType: any;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  navigateToResponseType(): void {
+    let fieldName = this.fieldType.name || this.fieldType.ofType.name || this.fieldType.ofType.ofType.name;
+    this.router.navigate(['documentation/graph2/type', fieldName]);
   }
 }
