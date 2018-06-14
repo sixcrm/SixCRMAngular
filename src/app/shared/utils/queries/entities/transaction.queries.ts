2b3e5578d44c46e007029a6cc723d6cd9b72d549
Nikola Bosic <nikola.bosic@coingcs.com>
2018-06-14 12:33:41 +0200

    Add type field to transaction

--- src/app/shared/utils/queries/entities/transaction.queries.ts
+++ src/app/shared/utils/queries/entities/transaction.queries.ts
@@ -51,16 +51,16 @@ export function refundTransactionMutation(transactionId: string, refundAmount: s
 }
 
 export function transactionInfoResponseQuery(): string {
-  return 'id amount alias created_at updated_at merchant_provider { id name } processor_response';
+  return 'id amount alias type created_at updated_at merchant_provider { id name } processor_response';
 }
 
 export function transactionCustomerInfoResponseQuery(): string {
-  return 'id amount alias created_at updated_at processor_response rebill { parentsession { id alias } } merchant_provider { id name }';
+  return 'id amount alias created_at updated_at processor_response type rebill { parentsession { id alias } } merchant_provider { id name }';
 }
 
 export function transactionResponseQuery(): string {
   return `
-    id alias amount processor_response created_at updated_at,
+    id alias amount processor_response type created_at updated_at,
     merchant_provider { id name }
     rebill { id amount }
     products { amount,
