58eba5202ecb7cc9dacb1eddcca2fec0af255871
Nikola Bosic <nikola.bosic@coingcs.com>
2018-08-08 15:30:20 +0200

    Remove redundand requests on dashboard, fix latest transactions table on dashboard, update icons

--- src/app/pages/dashboard-page/dashboard-availability.service.ts
+++ src/app/pages/dashboard-page/dashboard-availability.service.ts
@@ -3,51 +3,60 @@ import {BehaviorSubject, Subscription} from 'rxjs';
 import { AuthenticationService } from '../../authentication/authentication.service';
 import { TransactionsService } from '../../entity-services/services/transactions.service';
 import { DashboardType } from './dashboard-type';
-import { Transaction } from '../../shared/models/transaction.model';
 import { utc } from 'moment';
 
 @Injectable()
 export class DashboardAvailabilityService {
 
   public availableDashboards: BehaviorSubject<DashboardType[]> = new BehaviorSubject<DashboardType[]>([]);
-  private sevenDaysAgo = utc().subtract(7, 'd');
-  private eightDaysAgo = utc().subtract(8, 'd');
 
-  private subscription: Subscription;
+  private fullSub: Subscription;
+  private lowSub: Subscription;
 
   constructor(
     private authService: AuthenticationService,
     private transactionService: TransactionsService
   ) {
 
-    this.authService.activeAcl$.subscribe(() => {
-      this.determineAvailableTypes();
+    this.authService.activeAcl$.subscribe((acl) => {
+      if (acl && acl.id) {
+        this.determineAvailableTypes();
+      }
     });
   }
 
   determineAvailableTypes() {
-    if (this.subscription) {
-      this.subscription.unsubscribe();
+    if (this.fullSub) {
+      this.fullSub.unsubscribe();
     }
 
-    this.subscription = this.transactionService.getEntitiesForDashboardCheck().subscribe((response: Transaction[]) => {
-      this.availableDashboards.next([DashboardType.setup]);
+    if (this.lowSub) {
+      this.lowSub.unsubscribe();
+    }
 
-      if (response.length > 0) {
-        this.availableDashboards.next([DashboardType.setup, DashboardType.low_data]);
-      }
+    this.checkDashboards();
+  }
 
-      let olderThan7Days = transaction => transaction.createdAt.isBefore(this.sevenDaysAgo);
-      let olderTrasactions = response.filter(olderThan7Days);
+  checkDashboards() {
+    this.fullSub = this.transactionService.haveEntitiesBefore(utc().subtract(7, 'd')).subscribe((hasOld: boolean) => {
 
-      if (olderTrasactions.length > 0 || response.length > 10) {
+      if (hasOld) {
         this.availableDashboards.next([DashboardType.setup, DashboardType.low_data, DashboardType.full]);
+      } else {
+        this.checkIfAny();
       }
 
     });
+  }
 
-    this.transactionService.resetPagination();
-    this.transactionService.transactionsUntil(this.eightDaysAgo);
+  checkIfAny() {
+    this.lowSub = this.transactionService.haveAnyEntities().subscribe((hasAny: boolean) => {
+      if (hasAny) {
+        this.availableDashboards.next([DashboardType.setup, DashboardType.low_data]);
+      } else {
+        this.availableDashboards.next([DashboardType.setup]);
+      }
+    })
   }
 
 }
