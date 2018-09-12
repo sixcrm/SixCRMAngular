5418ef952efe0314db232a10e4e79e9a540a09b5
Carlos Casarez <carlitoescobar@gmail.com>
2018-09-12 14:25:45 -0700

    added search to protractor temp suite

--- protractor.conf.js
+++ protractor.conf.js
@@ -72,7 +72,7 @@ exports.config = {
       './e2e/**/navigation.e2e-spec.ts'
     ],
     temp: [
-      './e2e/**/campaigns.e2e-spec.ts'
+      './e2e/**/search.e2e-spec.ts'
     ]
   },
   capabilities: {
