3137a5a32c10ea6c48f90ecb0496ba5fb2dd5aa9
Nikola Bosic <nikola.bosic@coingcs.com>
2018-09-28 13:07:27 +0200

    Add subscriptions e2e suite

--- protractor.conf.js
+++ protractor.conf.js
@@ -61,6 +61,7 @@ exports.config = {
     './e2e/**/merchant-provider-group.e2e-spec.ts',
     './e2e/**/transaction.e2e-spec.ts',
     './e2e/**/order.e2e-spec.ts',
+    './e2e/**/subscription.e2e-spec.ts',
     './e2e/**/email-template.e2e-spec.ts'
   ],
   suites:  {
