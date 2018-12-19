b5d0cbdb3301e9d2a0dd846d43a6f04a3fd12846
Nikola B <nikola.bosic@toptal.com>
2018-12-19 16:48:17 +0100

    temp remove of product e2e from smoke test

--- protractor.conf.js
+++ protractor.conf.js
@@ -71,8 +71,7 @@ exports.config = {
       './e2e/**/register-behaviors.e2e-spec.ts',
       './e2e/**/accept-invite.e2e-spec.ts',
       './e2e/**/create-order.e2e-spec.ts',
-      './e2e/**/navigation.e2e-spec.ts',
-      './e2e/**/product-and-schedule.e2e-spec.ts'
+      './e2e/**/navigation.e2e-spec.ts'
     ],
     temp: [
       './e2e/**/campaigns.e2e-spec.ts'
