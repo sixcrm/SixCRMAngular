3f93b5800f04a513c00c7b28e9e5f20b0e4fda12
Nikola Bosic <nikola.bosic@coingcs.com>
2019-01-23 15:23:14 +0100

    Update routing animation

--- src/app/routing.animations.ts
+++ src/app/routing.animations.ts
@@ -8,11 +8,11 @@ export function slideToLeft() {
   return trigger('routerTransition', [
     transition(':enter', [
       style({transform: 'translateX(85%)', position: 'fixed', width: '100%'}),
-      animate('0.335ms ease-in-out', style({transform: 'translateX(0%)'}))
+      animate('335ms ease-in-out', style({transform: 'translateX(0%)'}))
     ]),
     transition(':leave', [
       style({transform: 'translateX(0%)', position: 'fixed', width: '100%'}),
-      animate('0.335ms ease-in-out', style({transform: 'translateX(-100%)'}))
+      animate('335ms ease-in-out', style({transform: 'translateX(-100%)'}))
     ])
   ]);
 }
@@ -23,11 +23,11 @@ export function slideToLeftAbsolute() {
     state('*', style({position: 'absolute', width: '100%'}) ),
     transition(':enter', [
       style({transform: 'translateX(100%)'}),
-      animate('0.335ms ease-in-out', style({transform: 'translateX(0%)'}))
+      animate('335ms ease-in-out', style({transform: 'translateX(0%)'}))
     ]),
     transition(':leave', [
       style({transform: 'translateX(0%)'}),
-      animate('0.335ms ease-in-out', style({transform: 'translateX(-100%)'}))
+      animate('335ms ease-in-out', style({transform: 'translateX(-100%)'}))
     ])
   ]);
 }
