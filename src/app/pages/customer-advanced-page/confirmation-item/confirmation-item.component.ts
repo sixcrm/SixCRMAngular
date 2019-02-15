4d02b1eb60a76e38f1e3da0e3d3488a13f12f085
Nikola B <nikola.bosic@toptal.com>
2019-02-15 10:18:54 +0100

    Add confirmation dialog on trial delivery/confirmation actions

--- src/app/pages/customer-advanced-page/confirmation-item/confirmation-item.component.ts
+++ src/app/pages/customer-advanced-page/confirmation-item/confirmation-item.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 import {Session} from '../../../shared/models/session.model';
+import {MatDialog} from '@angular/material';
+import {DeleteDialogComponent} from '../../../dialog-modals/delete-dialog.component';
 
 @Component({
   selector: 'confirmation-item',
@@ -13,16 +15,29 @@ export class ConfirmationItemComponent implements OnInit {
   @Output() confirmDelivery: EventEmitter<Session> = new EventEmitter();
   @Output() confirmTrial: EventEmitter<Session> = new EventEmitter();
 
-  constructor() { }
+  constructor(private dialog: MatDialog) { }
 
   ngOnInit() {
   }
 
   confirm() {
-    if (this.sessionConfirmation.trialConfirmation.delivered_at) {
-      this.confirmTrial.emit(this.sessionConfirmation);
-    } else {
-      this.confirmDelivery.emit(this.sessionConfirmation);
-    }
+    const isDeliveryConfirmation = !this.sessionConfirmation.trialConfirmation.delivered_at;
+
+    let ref = this.dialog.open(DeleteDialogComponent);
+    ref.componentInstance.text = `${isDeliveryConfirmation ? 'Delivery': 'Trial'} Confirmation`;
+    ref.componentInstance.secondaryText = `Are you sure you want to confirm ${isDeliveryConfirmation ? `delivery of` : ''} ${this.sessionConfirmation.alias}?`;
+
+    ref.afterClosed().subscribe(result => {
+      ref = null;
+
+      if (result && result.success) {
+        if (isDeliveryConfirmation) {
+          this.confirmDelivery.emit(this.sessionConfirmation);
+        } else {
+          this.confirmTrial.emit(this.sessionConfirmation);
+        }
+      }
+
+    })
   }
 }
