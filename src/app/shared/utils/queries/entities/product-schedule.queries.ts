bf46fa4036cb28239f76c4ffffe347fbaac7d171
Nikola Bosic <nikola.bosic@coingcs.com>
2019-03-20 15:52:22 +0100

    Fix product grid schedule price

--- src/app/shared/utils/queries/entities/product-schedule.queries.ts
+++ src/app/shared/utils/queries/entities/product-schedule.queries.ts
@@ -55,7 +55,7 @@ export function productScheduleResponseQuery(): string {
 }
 
 export function productScheduleInfoResponseQuery(): string {
-  return `id name created_at updated_at cycles { length }`
+  return `id name created_at updated_at cycles { length price }`
 }
 
 export function productScheduleInputQuery(productSchedule: ProductSchedule, includeId?: boolean): string {
