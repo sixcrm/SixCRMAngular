c89b2f635946ea00ac92930c18fd3fdadef2f168
Nikola B <nikola.bosic@toptal.com>
2019-04-03 09:30:35 +0100

    Add more test cases (product schedule) on create order e2e

--- e2e/specs/create-order.e2e-spec.ts
+++ e2e/specs/create-order.e2e-spec.ts
@@ -178,7 +178,7 @@ describe('New Order', function() {
   });
 
   it('should display proper product summary', () => {
-    expect(createOrderPage.getProductItems().get(0).getText()).toContain('Create Order Product');
+    expect(createOrderPage.getProductName(0).getText()).toContain('Create Order Product');
   });
 
   it('should submit the order and display processing overlay', () => {
@@ -195,5 +195,157 @@ describe('New Order', function() {
   it('should close modal on success', () => {
     createOrderPage.getOrderCompleteDoneButton().click();
     expectNotPresent(createOrderPage.getCreateOrderModalContainer());
-  })
+  });
+
+  it('should open create order modal again and select a product schedule', () => {
+    browser.sleep(2000);
+    topnav.getAddButton().click();
+    topnav.getDropdownOptions().first().click();
+
+    browser.sleep(2000);
+
+    createOrderPage.getCustomerNewButton().click();
+
+    createOrderPage.getNewCustomerInputs().get(0).sendKeys('Test Customer FN');
+    createOrderPage.getNewCustomerInputs().get(1).sendKeys('Test Customer LN');
+    createOrderPage.getNewCustomerInputs().get(2).sendKeys('(123) 123-1231');
+    createOrderPage.getNewCustomerInputs().get(3).sendKeys(`e2e-customer-${generateString(5)}@sixcrm.com`);
+
+    createOrderPage.getCustomerNextButton().click();
+    browser.sleep(500);
+
+    createOrderPage.getCampaignNextButton().click();
+    browser.sleep(500);
+
+    createOrderPage.getProductsInputSelector().click();
+    browser.sleep(500);
+    createOrderPage.getMenuOption(0).click();
+    browser.sleep(500);
+
+    createOrderPage.getProductsNextButton().click();
+
+    createOrderPage.getNewAddressInputs().get(0).sendKeys('1 test');
+    createOrderPage.getNewAddressInputs().get(1).sendKeys('line two');
+    createOrderPage.getNewAddressInputs().get(2).sendKeys('New York');
+    createOrderPage.getNewAddressInputs().get(3).sendKeys('21000');
+    createOrderPage.getNewAddressInputs().get(4).click();
+    browser.sleep(500);
+    createOrderPage.getFirstMenuOption().click();
+    createOrderPage.getNewAddressInputs().get(5).click();
+    browser.sleep(500);
+    createOrderPage.getFirstMenuOption().click();
+    createOrderPage.getAddressNextButton().click();
+    browser.sleep(500);
+
+    createOrderPage.getBillingInputs().get(0).sendKeys('4242424242424242');
+    createOrderPage.getBillingInputs().get(1).sendKeys('123');
+    createOrderPage.getBillingInputs().get(2).sendKeys('Test Customer');
+    createOrderPage.getBillingInputs().get(3).click();
+    browser.sleep(200);
+    createOrderPage.getDateDropdownOptions().get(6).click();
+    createOrderPage.getBillingInputs().get(4).click();
+    browser.sleep(200);
+    createOrderPage.getDateDropdownOptions().get(4).click();
+    createOrderPage.getAddressCheckbox().click();
+
+    createOrderPage.getBillingNextButton().click();
+    browser.sleep(500);
+
+    expect(createOrderPage.getProductName(0).getText()).toContain('A Subscription');
+
+    createOrderPage.getSubmitButton().click();
+    browser.sleep(500);
+
+    expectPresent(createOrderPage.getProcessingOverlay());
+  });
+
+  it('should show order success details for product schedule', () => {
+    waitForPresenceOf(createOrderPage.getOrderCompleteContainer());
+    expectPresent(createOrderPage.getOrderCompleteContainer());
+  });
+
+  it('should close modal on success for product schedule', () => {
+    createOrderPage.getOrderCompleteDoneButton().click();
+    expectNotPresent(createOrderPage.getCreateOrderModalContainer());
+  });
+
+
+  it('should open create order modal again and select a product and a product schedule', () => {
+    browser.sleep(2000);
+    topnav.getAddButton().click();
+    topnav.getDropdownOptions().first().click();
+
+    browser.sleep(2000);
+
+    createOrderPage.getCustomerNewButton().click();
+
+    createOrderPage.getNewCustomerInputs().get(0).sendKeys('Test Customer FN');
+    createOrderPage.getNewCustomerInputs().get(1).sendKeys('Test Customer LN');
+    createOrderPage.getNewCustomerInputs().get(2).sendKeys('(123) 123-1231');
+    createOrderPage.getNewCustomerInputs().get(3).sendKeys(`e2e-customer-${generateString(5)}@sixcrm.com`);
+
+    createOrderPage.getCustomerNextButton().click();
+    browser.sleep(500);
+
+    createOrderPage.getCampaignNextButton().click();
+    browser.sleep(500);
+
+    createOrderPage.getProductsInputSelector().click();
+    browser.sleep(500);
+    createOrderPage.getMenuOption(0).click();
+    browser.sleep(500);
+
+    createOrderPage.getProductsInputSelector().click();
+    browser.sleep(500);
+    createOrderPage.getMenuOption(0).click();
+    browser.sleep(500);
+
+    createOrderPage.getProductsNextButton().click();
+
+    createOrderPage.getNewAddressInputs().get(0).sendKeys('1 test');
+    createOrderPage.getNewAddressInputs().get(1).sendKeys('line two');
+    createOrderPage.getNewAddressInputs().get(2).sendKeys('New York');
+    createOrderPage.getNewAddressInputs().get(3).sendKeys('21000');
+    createOrderPage.getNewAddressInputs().get(4).click();
+    browser.sleep(500);
+    createOrderPage.getFirstMenuOption().click();
+    createOrderPage.getNewAddressInputs().get(5).click();
+    browser.sleep(500);
+    createOrderPage.getFirstMenuOption().click();
+    createOrderPage.getAddressNextButton().click();
+    browser.sleep(500);
+
+    createOrderPage.getBillingInputs().get(0).sendKeys('4242424242424242');
+    createOrderPage.getBillingInputs().get(1).sendKeys('123');
+    createOrderPage.getBillingInputs().get(2).sendKeys('Test Customer');
+    createOrderPage.getBillingInputs().get(3).click();
+    browser.sleep(200);
+    createOrderPage.getDateDropdownOptions().get(6).click();
+    createOrderPage.getBillingInputs().get(4).click();
+    browser.sleep(200);
+    createOrderPage.getDateDropdownOptions().get(4).click();
+    createOrderPage.getAddressCheckbox().click();
+
+    createOrderPage.getBillingNextButton().click();
+    browser.sleep(500);
+
+    expect(createOrderPage.getProductName(0).getText()).toContain('A Subscription');
+    expect(createOrderPage.getProductName(1).getText()).toContain('Create Order Product');
+
+    createOrderPage.getSubmitButton().click();
+    browser.sleep(500);
+
+    expectPresent(createOrderPage.getProcessingOverlay());
+  });
+
+  it('should show order success details for a product and a product schedule', () => {
+    waitForPresenceOf(createOrderPage.getOrderCompleteContainer());
+    expectPresent(createOrderPage.getOrderCompleteContainer());
+  });
+
+  it('should close modal on success for a product and a product schedule', () => {
+    createOrderPage.getOrderCompleteDoneButton().click();
+    expectNotPresent(createOrderPage.getCreateOrderModalContainer());
+  });
+
 });
